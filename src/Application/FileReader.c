207f92a7473ed428a96926fe56cc96c201cfb496
Resonance4K <Resonance4K@gmail.com>
2019-04-22 15:54:14 +0800

    Separated directory reading logic into multiple functions

--- src/Application/FileReader.c
+++ src/Application/FileReader.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <dirent.h>
 #include <string.h>
@@ -11,6 +12,10 @@
 
 void ReadDirectory(const char *const path, const unsigned int depth);
 DIR * GetDirectory(const char *const path);
+bool IsValidDirectory(DIR * dir);
+bool IsRootDirectory(const unsigned int depth);
+char * GetFullPath(const char *const path, const char *const entry_name);
+
 void ReadFile(const char *const path);
 FILE * GetFile(const char *const path, const char *const mode);
 
@@ -21,7 +26,7 @@ FILE * GetFile(const char *const path, const char *const mode);
 
 
 // Flag that keeps track of whether the root directory that was provided by the user is valid.
-bool IsValidRootDirectory = false;
+bool IsValidRootDirectory = true;
 
 // Defined in "FileReader.h".
 void ReadRootDirectory(const char *const path)
@@ -34,34 +39,41 @@ void ReadDirectory(const char *const path, const unsigned int depth)
 {
 	DIR * dir = GetDirectory(path);
 
-	if (dir == NULL && depth == ROOT_DEPTH)
+	if (!IsValidDirectory(dir))
 	{
-		printf("[ERROR] Not a valid directory: %s\n", path);
+		if (IsRootDirectory(depth))
+		{
+			IsValidRootDirectory = false;
+			printf("[ERROR] Not a valid directory: %s\n", path);
+		}
+
 		return;
 	}
 	
 	struct dirent * ent;
 	while ((ent = readdir(dir)) != NULL)
 	{
-		if (strcmp(ent->d_name, ".") == 0 || strcmp(ent->d_name, "..") == 0) { continue; }
+		const char *const entry_name = ent->d_name;
+		const unsigned int entry_type = ent->d_type;
 
-		char fullpath[1024];
-		strcpy(fullpath, path);
-		strcat(fullpath, "/");
-		strcat(fullpath, ent->d_name);
+		if (strcmp(entry_name, ".") == 0 || strcmp(entry_name, "..") == 0) { continue; }
+
+		const char *const fullpath = GetFullPath(path, entry_name);
 		
 		printf("%s\n", fullpath);
 		printf("[DEPTH=%i]\n", depth);
 
-		if (ent->d_type == DT_DIR)
+		if (entry_type == DT_DIR)
 		{
 			printf("[DIR]\n");
 			ReadDirectory(fullpath, depth + 1);
 		}
-		else if (ent->d_type == DT_REG)
+		else if (entry_type == DT_REG)
 		{
 			printf("[REG]\n");
 		}
+
+		free(fullpath);
 	}
 
 	closedir(dir);
@@ -71,6 +83,36 @@ void ReadDirectory(const char *const path, const unsigned int depth)
 DIR * GetDirectory(const char *const path)
 {
 	return opendir(path);
+}
+
+// Returns true if the directory being read is valid (not NULL) and false otherwise.
+bool IsValidDirectory(DIR * dir)
+{
+	return dir != NULL;
+}
+
+// Returns true if the directory being read is the root directory and false otherwise.
+bool IsRootDirectory(const unsigned int depth)
+{
+	return depth == ROOT_DEPTH;
+}
+
+// Returns the full path resulting from a concatenation of the path and entry name variables.
+// NOTE: This allocates memory on the heap which must be freed after use.
+char * GetFullPath(const char *const path, const char *const entry_name)
+{
+	const unsigned int path_length = strlen(path);
+	const unsigned int entry_name_length = strlen(entry_name);
+
+	const unsigned int length = path_length + entry_name_length + 1;
+
+	char *const fullpath = malloc( length * sizeof(*fullpath) );
+
+	strcpy(fullpath, path);
+	strcat(fullpath, "/");
+	strcat(fullpath, entry_name);
+
+	return fullpath;
 }
 
 // Reads a file specified by the path and processes it if it is a supported source file.
