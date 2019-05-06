f3a3a64e0c4487fa1b4c84beceeeefe177909153
Resonance4K <Resonance4K@gmail.com>
2019-05-06 20:15:36 +0800

    Added placeholder reading functions for source files (Java, C, C++) and header files (C/C++)

--- src/Application/Readers.c
+++ src/Application/Readers.c
@@ -1,2 +1,32 @@
 
 #include "Application/Readers.h"
+
+
+// --- Constant Definitions --- //
+
+#define MAX_LINE_LENGTH    512
+
+
+// Defined in "Readers.h".
+void ReadFile_JAVA(FILE * file)
+{
+
+}
+
+// Defined in "Readers.h".
+void ReadFile_C(FILE * file)
+{
+
+}
+
+// Defined in "Readers.h".
+void ReadFile_CPP(FILE * file)
+{
+
+}
+
+// Defined in "Readers.h".
+void ReadFile_HEADER(FILE * file)
+{
+
+}
--- src/Application/Readers.h
+++ src/Application/Readers.h
@@ -3,7 +3,20 @@
 #define SFR_READERS_H
 
 
+#include <stdio.h>
 
+
+// Reads a source file that is written in Java.
+void ReadFile_JAVA(FILE * file);
+
+// Reads a source file that is written in C.
+void ReadFile_C(FILE * file);
+
+// Reads a source file that is written in C++.
+void ReadFile_CPP(FILE * file);
+
+// Reads a header file that is written in C/C++.
+void ReadFile_HEADER(FILE * file);
 
 
 #endif
