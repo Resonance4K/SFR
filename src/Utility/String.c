f02d06969dad41017cd16b297dd119bdaccb92e4
Resonance4K <Resonance4K@gmail.com>
2019-05-06 20:14:32 +0800

    Added formatting functions (ToUpperCase, ToLowerCase, Substr) and search functions (FindBack)

--- src/Utility/String.c
+++ src/Utility/String.c
@@ -1,4 +1,7 @@
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "Utility/String.h"
 
 
@@ -16,4 +19,75 @@ void StrReplaceAll(char *const string, const char target, const char replacement
 
 		index++;
 	}
+}
+
+// Defined in "String.h".
+void StrToLowerCase(char *const string)
+{
+	unsigned int index = 0;
+
+	while (*(string + index) != '\0')
+	{
+		const unsigned int cInt = (unsigned int) *(string + index);
+		const unsigned int cOffset = (unsigned int) ('a' - 'A');
+
+		if (cInt >= 'A' && cInt <= 'Z')
+		{
+			*(string + index) = (char) (cInt + cOffset);
+		}
+
+		index++;
+	}
+}
+
+// Defined in "String.h".
+void StrToUpperCase(char *const string)
+{
+	unsigned int index = 0;
+
+	while (*(string + index) != '\0')
+	{
+		const unsigned int cInt = (unsigned int) *(string + index);
+		const unsigned int cOffset = (unsigned int) ('a' - 'A');
+
+		if (cInt >= 'a' && cInt <= 'z')
+		{
+			*(string + index) = (char) (cInt - cOffset);
+		}
+
+		index++;
+	}
+}
+
+// Defined in "String.h".
+int StrFindBack(const char *const string, const char target)
+{
+	for (int i = (strlen(string) - 1); i >= 0; i--)
+	{
+		if (*(string + i) == target)
+		{
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+// Defined in "String.h".
+char * StrSubstr(const char *const string, const unsigned int start, const unsigned int end)
+{
+	if (end < start) { return NULL; }
+	if (start > (strlen(string) - 1)) { return NULL; }
+
+	const unsigned int substr_length = (end > strlen(string) ? (strlen(string) - start) : (end - start));
+
+	char *const substr = malloc( substr_length * sizeof( *substr ) + 1 );
+
+	for (unsigned int i = 0; i < substr_length; i++)
+	{
+		*(substr + i) = *(string + start + i);
+	}
+	*(substr + substr_length) = '\0';
+
+	return substr;
 }
--- src/Utility/String.h
+++ src/Utility/String.h
@@ -6,5 +6,20 @@
 // Modifies the string by replacing all target characters with the replacement character.
 void StrReplaceAll(char *const string, const char target, const char replacement);
 
+// Modifies the string by turning all uppercase characters into lowercase characters.
+void StrToLowerCase(char *const string);
+
+// Modifies the string by turning all lowercase characters into uppercase characters.
+void StrToUpperCase(char *const string);
+
+// Searches the string from end to start in order to find the target character.
+// Returns the index of the target character or -1 if the target character could not be found.
+int StrFindBack(const char *const string, const char target);
+
+// Extracts the characters of the string between the start index (inclusive) and end index (exclusive).
+// Returns a substring of the string or NULL if there is no valid substring.
+// NOTE: This allocates memory on the heap which must be freed after use.
+char * StrSubstr(const char *const string, const unsigned int start, const unsigned int end);
+
 
 #endif
