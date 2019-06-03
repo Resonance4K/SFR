cc21999005461caf504c5d6b4bbb1f6edb94b9c0
Resonance4K <Resonance4K@gmail.com>
2019-06-03 22:55:40 +0800

    Added a function to read C# source files and a struct to store statistical information about the files being read

--- src/Application/Readers.c
+++ src/Application/Readers.c
@@ -6,6 +6,33 @@
 
 #define MAX_LINE_LENGTH    512
 
+#define JAVA        0
+#define C           1
+#define CPP         2
+#define HEADER      3
+#define CS          4
+
+
+// Defined in "Readers.h".
+struct FILE_DATA data[TOTAL_FILES_SUPPORTED];
+
+// Defined in "Readers.h".
+void InitReaders(void)
+{
+	data[JAVA].file_type = "Java";
+	data[C].file_type = "C";
+	data[CPP].file_type = "C++";
+	data[HEADER].file_type = "C/C++ Header";
+	data[CS].file_type = "C#";
+
+	for (int i = 0; i < TOTAL_FILES_SUPPORTED; i++)
+	{
+		data[i].code_lines = 0;
+		data[i].empty_lines = 0;
+		data[i].doc_lines = 0;
+		data[i].file_count = 0;
+	}
+}
 
 // Defined in "Readers.h".
 void ReadFile_JAVA(FILE * file)
@@ -27,6 +54,12 @@ void ReadFile_CPP(FILE * file)
 
 // Defined in "Readers.h".
 void ReadFile_HEADER(FILE * file)
+{
+
+}
+
+// Defined in "Readers.h".
+void ReadFile_CS(FILE * file)
 {
 
 }
--- src/Application/Readers.h
+++ src/Application/Readers.h
@@ -6,6 +6,27 @@
 #include <stdio.h>
 
 
+// --- Constant Definitions --- //
+
+#define TOTAL_FILES_SUPPORTED    5
+
+
+// Structure that stores statistical information about the files being read.
+struct FILE_DATA
+{
+	const char * file_type;
+	unsigned long code_lines;
+	unsigned long empty_lines;
+	unsigned long doc_lines;
+	unsigned long file_count;
+};
+
+// Array to store the file data information for each file type.
+extern struct FILE_DATA data[];
+
+// Initializes the properties of the readers.
+void InitReaders(void);
+
 // Reads a source file that is written in Java.
 void ReadFile_JAVA(FILE * file);
 
@@ -17,6 +38,9 @@ void ReadFile_CPP(FILE * file);
 
 // Reads a header file that is written in C/C++.
 void ReadFile_HEADER(FILE * file);
+
+// Reads a source file that is written in C#.
+void ReadFile_CS(FILE * file);
 
 
 #endif
